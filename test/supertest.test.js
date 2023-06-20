addb72faf0d2ae79f621bf17b5654b089e9c95e2
PedroZaja <pedrodavid21@gmail.com>
2023-06-20 20:45:14 -0300

    entrega de super test

---
+++ test/supertest.test.js
@@ -0,0 +1,11 @@
+import chai from "chai";
+import supertest from "supertest";
+
+const expect = chai.expect;
+const requester = supertest("http://localhost:8080");
+
+describe ("Testing de la app", () => {
+    describe ("Testing Api", () => {
+        
+    });
+});
